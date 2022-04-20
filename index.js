f08ef58b68f357a6401b22ce2b464d2d147e3873
nickxbs <febbrari@gmail.com>
2022-04-20 22:51:19 +0200

    export upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const surprise = require('./src/surprise');
 const main = require('./src/main');
 const downloads = require('./src/downloads');
+const upload = require('./src/upload-cgmsim');
 
 
 
 
-module.exports = { surprise, main, downloads };
+module.exports = { surprise, main, downloads,upload };
