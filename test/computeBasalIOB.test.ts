7bae20285458a60dcfb42642a6f1836df05135af
nickxbs <febbrari@gmail.com>
2022-05-11 21:54:27 +0200

    Fix tests

--- test/computeBasalIOB.test.ts
+++ test/computeBasalIOB.test.ts
@@ -3,6 +3,16 @@ import computeBasalIOB from "../src/computeBasalIOB";
 import { Treatment } from "../src/Types";
 
 describe('test computeBasalIOB', () => {
+
+	beforeAll(() => {
+		jest.useFakeTimers('modern');
+		jest.setSystemTime(new Date('2022-05-07T11:20:00'));
+	});
+
+	afterAll(() => {
+		jest.useRealTimers();
+	});
+
 	test('detection drug', () => {
 		const result = computeBasalIOB(treatments as unknown as Treatment[]);
 
--- test/perlin.test.ts
+++ test/perlin.test.ts
@@ -1,5 +1,5 @@
 import perlin from "../src/perlin";
-describe('test perlin', () => {
+describe('test perlin with fake timers', () => {
 	beforeAll(() => {
 		jest.useFakeTimers('modern');
 		jest.setSystemTime(new Date('2001-01-01'));
@@ -16,12 +16,32 @@ describe('test perlin', () => {
 		expect(perl[0].time).toBe(perl2[0].time)
 		expect(perl[0].noise).toBe(perl2[0].noise)
 	})
+	test('run twice different day should return different array', () => {
+		const perl = perlin()
+		jest.setSystemTime(new Date('2001-01-02T01:01'));
+		const perl2 = perlin()
+		expect(perl[0].time).not.toBe(perl2[0].time)
+		expect(perl[0].noise).not.toBe(perl2[0].noise)
+	})
 	
-	test('run in same should return same array', () => {
+	test('run in same day should return same array', () => {
 		const perl = perlin()
 		expect(perl.length).toBe(288);
 		expect(perl[0]).not.toBe(perl[1])
 
 		expect(perl).toMatchSnapshot()
 	})
+})
+
+describe('test perlin with real timers', () => {
+
+	test('run twice in same day should return same array', () => {
+		const perl = perlin()
+		setTimeout(() => {
+			const perl2 = perlin()
+			expect(perl[0].time).toBe(perl2[0].time)
+			expect(perl[0].noise).toBe(perl2[0].noise)
+		}, 100);
+	})
+
 })
