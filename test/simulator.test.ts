23d24edbd2c50b4c4521f72fa6aca6b041292ab9
Nicola Febbrari <febbrari@gmail.com>
2022-06-04 18:39:01 +0200

    add carb test

--- test/simulator.test.ts
+++ test/simulator.test.ts
@@ -17,8 +17,8 @@ describe('simulator test', () => {
 	});
 
 	test('start from 274 22:10 + 14u tou +', () => {
-		 let now = moment(date);
-		 
+		let now = moment(date);
+
 
 		const entries: Sgv[] = [{
 			mills: now.add(-5, 'minutes').toDate().getTime(),
@@ -64,6 +64,72 @@ describe('simulator test', () => {
 
 			const result = simulator({ env, entries, treatments, profiles: [] })
 
+			entries.splice(0, 0, {
+				mills: now.toDate().getTime(),
+				sgv: result.sgv,
+			})
+			expect(result.deltaMinutes).toBeGreaterThanOrEqual(0)
+			expect(result.basalActivity).toBeGreaterThanOrEqual(0)
+			expect(result.bolusActivity).toBeGreaterThanOrEqual(0)
+			expect(result.carbsActivity).toBeGreaterThanOrEqual(0)
+			expect(result.liverActivity).toBeGreaterThanOrEqual(0)
+			expect(result.pumpBasalActivity).toBeGreaterThanOrEqual(0)
+			// console.log('Result ' + result.sgv + ' ' + now.toLocaleString())
+			log.push('Result ' + result.sgv + ' ' + now.toISOString())
+
+			now = now.add(5, "minutes");
+			index = index + 5;
+			jest.setSystemTime(now.toDate());
+		}
+		expect(log).toMatchSnapshot();
+
+
+	})
+
+
+
+	test('start from 119 15:00 + 50g tou +', () => {
+		let now = moment('2022-06-04T13:00:00.000Z');
+		jest.setSystemTime(now.toDate());
+
+
+
+		const entries: Sgv[] = [{
+			mills: now.add(-5, 'minutes').toDate().getTime(),
+			sgv: 124
+		}, {
+			mills: now.add(-5, 'minutes').toDate().getTime(),
+			sgv: 125
+		}, {
+			mills: now.add(-5, 'minutes').toDate().getTime(),
+			sgv: 126
+		}, {
+			mills: now.add(-5, 'minutes').toDate().getTime(),
+			sgv: 127
+		}
+		];
+		now = moment('2022-06-04T13:00:00.000Z');
+		const treatments: Treatment[] = [{
+			created_at: now.toISOString(),
+			carbs: 50
+		},{
+			created_at: '2022-06-04T01:00:00.000Z',
+			notes: 'tou 41',
+			carbs: 0
+		}]
+		const env: EnvParam = {
+			CARBS_ABS_TIME: '360',
+			CR: '10',
+			DIA: '6',
+			ISF: '32',
+			TP: '75',
+			WEIGHT: '80',
+		}
+
+		const log = []
+		log.push('Meal 50g  ' + now.toISOString());
+		for (let index = 0; index < (60 * 34);) {
+			const result = simulator({ env, entries, treatments, profiles: [] })
 			entries.splice(0, 0, {
 				mills: now.toDate().getTime(),
 				sgv: result.sgv,
