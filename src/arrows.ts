f2f3c8fb8331298af4d025a4403bde12ef2cc9f5
Nicola Febbrari <febbrari@gmail.com>
2022-06-09 20:49:47 +0200

    Refactor arrows output

--- src/arrows.ts
+++ src/arrows.ts
@@ -4,8 +4,7 @@ import { Direction } from './Types';
 //const logger = pino();
 
 
-export default function (sgvLast, sgv1, sgv2, sgv3): { sgvdir: number; direction: Direction }[] {
-	const arrows = [];
+export default function (sgvLast, sgv1, sgv2, sgv3): { sgvdir: number; direction: Direction } {
 	if (sgvLast && sgv1 && sgv2 && sgv3) {
 		const sgvdir1 = sgvLast - sgv1;
 		const sgvdir2 = sgv1 - sgv2;
@@ -14,55 +13,52 @@ export default function (sgvLast, sgv1, sgv2, sgv3): { sgvdir: number; direction
 		logger.debug('this is the mean SGV 5 min variation in the last 15 minutes: %o', sgvdir15min, 'mg/dl');
 
 		if (sgvdir15min < -10) {
-			arrows.push({
+			return {
 				sgvdir: sgvdir15min,
 				direction: 'DoubleDown'
-			});
+			};
 
 		} else if (sgvdir15min < -6) {
-			arrows.push({
+			return {
 				sgvdir: sgvdir15min,
 				direction: 'SingleDown'
-			});
+			};
 
 		} else if (sgvdir15min < -2) {
-			arrows.push({
+			return {
 				sgvdir: sgvdir15min,
 				direction: 'FortyFiveDown'
-			});
+			};
 
 		} else if (sgvdir15min < 2) {
-			arrows.push({
+			return {
 				sgvdir: sgvdir15min,
 				direction: 'Flat'
-			});
+			};
 
 		} else if (sgvdir15min < 6) {
-			arrows.push({
+			return {
 				sgvdir: sgvdir15min,
 				direction: 'FortyFiveUp'
-			});
+			};
 
 		} else if (sgvdir15min < 10) {
-			arrows.push({
+			return {
 				sgvdir: sgvdir15min,
 				direction: 'SingleUp'
-			});
+			};
 
 		} else if (sgvdir15min >= 10) {
-			arrows.push({
+			return {
 				sgvdir: sgvdir15min,
 				direction: 'DoubleUp'
-			});
+			};
 		}
 
 	} else {
-		arrows.push({
+		return {
 			sgvdir: 0,
 			direction: 'Flat'
-		});
+		};
 	}
-	logger.info('New Arrows status result %o', arrows);
-
-	return arrows;
 }
