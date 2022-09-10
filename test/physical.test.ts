f4f88637f5d31a1077c2ed6d92ad18d713571319
Nicola Febbrari <febbrari@gmail.com>
2022-09-10 22:32:53 +0200

    rename test

--- test/physical.test.ts
+++ test/physical.test.ts
@@ -12,7 +12,7 @@ describe('Physical test', () => {
 	afterAll(() => {
 		jest.useRealTimers();
 	});
-	test('test flat hr =0.5', () => {
+	test('physicalIsf with flat hr =0.5', () => {
 		const activities = getFlatHeartRate({ heartRate: 170 * 0.5, created_at: '2001-01-01T00:00:00.000Z' }, 6);
 		const result = physicalIsf(activities);
 		jest.setSystemTime(new Date('2001-01-01T06:00:00.000Z'));
@@ -27,12 +27,12 @@ describe('Physical test', () => {
 	// 	expect(result).toBe(2.076371170507074);
 	// })
 
-	describe('sequence', () => {
+	describe('physicalIsf with 6h0.5 + 2h0.7 + 6h0.5', () => {
 		const activities05 = getFlatHeartRate({ heartRate: 170 * 0.5, created_at: '2001-01-01T00:00:00.000Z' }, 6);
 		const activities07 = getFlatHeartRate({ heartRate: 170 * 0.7, created_at: '2001-01-01T06:00:00.000Z' }, 2);
 		const activities05n = getFlatHeartRate({ heartRate: 170 * 0.5, created_at: '2001-01-01T08:00:00.000Z' }, 6);
 
-		test.each([...activities05, ...activities07, ...activities05n])('test sequence %p', (t) => {
+		test.each([...activities05, ...activities07, ...activities05n])('%p', (t) => {
 			jest.setSystemTime(new Date(t.created_at));
 			const result = physicalIsf([...activities05, ...activities07]);
 			expect(result).toMatchSnapshot();
