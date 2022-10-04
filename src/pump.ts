4b73597375a87abe4d51050bb5d40d5a6c79ae61
Nicola Febbrari <febbrari@gmail.com>
2022-10-04 21:55:53 +0200

    profile switch

--- src/pump.ts
+++ src/pump.ts
@@ -2,6 +2,51 @@ import logger, { getDeltaMinutes, getInsulinActivity } from './utils';
 import * as moment from 'moment';
 import { Profile, Treatment } from './Types';
 
+function getProfileSwitch(treatments, duration) {
+	const computedProfilesSwitch: { start: moment.Moment; end: moment.Moment; basal: { time: string, value: number }[] | number }[] = [];
+	const now = moment().utc();
+	treatments
+		.filter(e =>
+			e.created_at &&
+			now.diff(moment(e.created_at), 'minutes') <= duration && // temps basals set in the last 3 hours
+			e.eventType === 'Profile Switch' &&
+			e.duration != 0
+		)
+		.sort((f, s) => {
+			return moment(f.created_at).diff(s.created_at);
+		})
+		.forEach((tr) => {
+			if (tr.profileJson && tr.percentage) {
+				const start = moment(tr.created_at).utc();
+				const end = moment(tr.created_at).add(tr.duration, 'minutes').utc();
+				const profile = JSON.parse(tr.profileJson);
+				if (Array.isArray(profile.basal)) {
+					const basals = profile.basal.map(pb => ({
+						time: pb.time,
+						value: pb.value * (tr.percentage / 100)
+					}))
+					computedProfilesSwitch.push({
+						start,
+						basal: basals,
+						end
+					});
+				} else {
+					computedProfilesSwitch.push({
+						start,
+						basal: profile.basal * (tr.percentage / 100),
+						end
+					});
+				}
+			} else {
+				const currentIndex = computedProfilesSwitch.length - 1;
+				if (currentIndex >= 0) {
+					computedProfilesSwitch[currentIndex].end = moment(tr.created_at);
+				}
+			}
+		})
+	return computedProfilesSwitch;
+}
+
 function getTempBasal(treatments, duration) {
 	const computedTempBasal: { start: moment.Moment; end: moment.Moment; insulin: number; }[] = [];
 	const now = moment().utc();
@@ -34,27 +79,31 @@ function getTempBasal(treatments, duration) {
 	return computedTempBasal;
 }
 
-function getBasalFromProfile(orderedProfiles: Profile[], currentAction: moment.Moment) {
+function getBasalFromProfiles(orderedProfiles: Profile[], currentAction: moment.Moment) {
 	//last basal before the end
 	const activeProfiles = orderedProfiles.filter(p => moment(p.startDate).diff(currentAction) <= 0)
-	if(activeProfiles && activeProfiles.length>0){
-		const activeProfile=activeProfiles[0];
-		const activeProfileName=activeProfile.defaultProfile;
-		const activeProfileBasals=activeProfile.store[activeProfileName].basal;
-		if(Array.isArray(activeProfileBasals)){
-			const compatiblesBasalProfiles = activeProfileBasals.filter(b => {
-				return b.time.localeCompare(currentAction.format('HH:mm')) <= 0;
-			});
-			const index = compatiblesBasalProfiles.length - 1;
-			const currentBasal = compatiblesBasalProfiles[index];
-			return currentBasal.value;
-		}else{
-			return activeProfileBasals;
-		}
+	if (activeProfiles && activeProfiles.length > 0) {
+		const activeProfile = activeProfiles[0];
+		const activeProfileName = activeProfile.defaultProfile;
+		const activeProfileBasals = activeProfile.store[activeProfileName].basal;
+		return  activeBasalByTime(activeProfileBasals, currentAction);
 	}
 	return 0;
 }
 
+
+function activeBasalByTime(activeProfileBasals: { value: number; time: string; timeAsSecond?: number; }[]|number, currentAction: moment.Moment) {
+	if (Array.isArray(activeProfileBasals)) {
+		const compatiblesBasalProfiles = activeProfileBasals.filter(b => {
+			return b.time.localeCompare(currentAction.format('HH:mm')) <= 0;
+		});
+		const index = compatiblesBasalProfiles.length - 1;
+		const currentBasal = compatiblesBasalProfiles[index];
+		return currentBasal.value;
+	} else {
+		return activeProfileBasals;
+	}	
+}
 
 export default function (treatments: Treatment[], profiles: Profile[], dia: number, peak: number) {
 	const basalAsBoluses: { minutesAgo: number, insulin: number }[] = [];
@@ -68,20 +117,18 @@ export default function (treatments: Treatment[], profiles: Profile[], dia: numb
 			return profile.store[profileName]
 		})
 		.sort((first, second) => (moment(second.startDate).diff(moment(first.startDate))));
-	const lastProfile = orderedProfiles[0];
-	if (!lastProfile) {
-		return 0;
-	}
-	const lastProfileName = lastProfile.defaultProfile;
-
-	const defaultProfileBasals = lastProfile.store[lastProfileName].basal;
-
 
 	const computedTempBasal = getTempBasal(treatments, duration);
+	const computedProfileSwitch = getProfileSwitch(treatments, duration);
 
 	// const basalsToUpdate = [];
 	for (let currentAction = startDiaAction; currentAction.diff(endDiaAction) <= 0; currentAction.add(5, 'minutes')) {
 		const tempBasalActives = computedTempBasal
+			.filter(t => (
+				t.start.diff(currentAction) <= 0 &&
+				t.end.diff(currentAction) > 0
+			));
+		const profilesStichActives = computedProfileSwitch
 			.filter(t => (
 				t.start.diff(currentAction) <= 0 &&
 				t.end.diff(currentAction) > 0
@@ -93,9 +140,16 @@ export default function (treatments: Treatment[], profiles: Profile[], dia: numb
 				minutesAgo: getDeltaMinutes(currentAction.toISOString()),
 				insulin: tempBasalActives[0].insulin / 12,
 			};
-		} else {
-			let currentBasal = { value: getBasalFromProfile(orderedProfiles, currentAction) };
+		} else if (profilesStichActives.length > 0) {
 
+			const basal= activeBasalByTime(profilesStichActives[0].basal,currentAction);
+			basalToUpdate = {
+				minutesAgo: getDeltaMinutes(currentAction.toISOString()),
+				insulin: basal / 12,
+			};
+		} else {
+
+			let currentBasal = { value: getBasalFromProfiles(orderedProfiles, currentAction) };
 			basalToUpdate = {
 				minutesAgo: getDeltaMinutes(currentAction.toISOString()),
 				insulin: currentBasal.value / 12,
--- test/pump.test.ts
+++ test/pump.test.ts
@@ -1,7 +1,3 @@
-import { treatments } from "./inputTest";
-import bolus from "../src/bolus";
-import { Treatment } from "../src/Types";
-
 import moment = require("moment");
 import pump from "../src/pump";
 
@@ -9,6 +5,45 @@ const dia = 6;
 const peak = 90;
 
 
+const dynamicBasal = [
+	{
+		time: '00:00',
+		value: 1
+	},
+	{
+		time: '12:00',
+		value: 1.2
+	}, {
+		time: '14:00',
+		value: 0.8
+	}, {
+		time: '18:00',
+		value: 1
+	}
+];
+const profileSwitch = [
+	{
+		eventType: 'Profile Switch',
+		percentage: 130,
+		duration: 30,
+		created_at: '2022-05-06T13:00:00.000Z',
+		profileJson: JSON.stringify({
+			basal: dynamicBasal
+		})
+	}
+]
+const tmpBasal = [{
+	eventType: 'Temp Basal',
+	absolute: 0,
+	duration: 60,
+	created_at: '2022-05-06T11:00:00.000Z'
+}, {
+	eventType: 'Temp Basal',
+	absolute: 1.2,
+	duration: 30,
+	created_at: '2022-05-06T16:00:00.000Z'
+},
+];
 describe('test pump', () => {
 
 	const date = new Date('2022-05-07T11:20:00Z');
@@ -71,18 +106,7 @@ describe('test pump', () => {
 	test('static basal in profile and 2 different temp basal', () => {
 		let _date = moment('2022-05-06T16:30:00.000Z');
 		jest.setSystemTime(_date.toDate());
-		const treatments = [{
-			eventType: 'Temp Basal',
-			absolute: 0,
-			duration: 60,
-			created_at: '2022-05-06T11:00:00.000Z'
-		}, {
-			eventType: 'Temp Basal',
-			absolute: 1.2,
-			duration: 30,
-			created_at: '2022-05-06T16:00:00.000Z'
-		},
-		]
+		const treatments = tmpBasal
 		const result = pump(treatments, [{
 			startDate: '2000-01-01T00:00:00.000Z',
 			defaultProfile: 'Default',
@@ -104,21 +128,40 @@ describe('test pump', () => {
 			defaultProfile: 'Default',
 			store: {
 				Default: {
-					basal: [
-						{
-							time: '00:00',
-							value: 1
-						}
-						, {
-							time: '12:00',
-							value: 1.2
-						}, {
-							time: '14:00',
-							value: 0.8
-						}, {
-							time: '18:00',
-							value: 1
-						}]
+					basal: dynamicBasal
+				}
+			}
+		}], dia, peak);
+
+		expect(result).toMatchSnapshot();
+	});
+	test('dynamic  basal in profile, no temp basal and profile switch', () => {
+		let _date = moment('2022-05-06T16:30:00.000Z');
+		jest.setSystemTime(_date.toDate());
+		const treatments = profileSwitch
+		const result = pump(treatments, [{
+			startDate: '2000-01-01T00:00:00.000Z',
+			defaultProfile: 'Default',
+			store: {
+				Default: {
+					basal: dynamicBasal
+				}
+			}
+		}], dia, peak);
+
+		expect(result).toMatchSnapshot();
+	});
+	test('dynamic basal in profile and temp basal', () => {
+		let _date = moment('2022-05-06T16:30:00.000Z');
+		jest.setSystemTime(_date.toDate());
+		const treatments = tmpBasal
+
+		const result = pump(treatments, [{
+			startDate: '2000-01-01T00:00:00.000Z',
+			defaultProfile: 'Default',
+			store: {
+				Default: {
+					basal: dynamicBasal
 				}
 			}
 		}], dia, peak);
