d67dc2bade31e516ad73b4753c4d6123393f1672
Nicola Febbrari <febbrari@gmail.com>
2022-10-05 23:45:27 +0200

    fix

--- src/Types.ts
+++ src/Types.ts
@@ -113,8 +113,7 @@ export type SimulationResult = {
 	bolusActivity: number;
 	noiseActivity: number;
 	liverActivity: number;
-	activityFactor: number;
-	notes:string;
+	activityFactor: number;	
 	isf: { dynamic: number; constant: number }
 };
 
