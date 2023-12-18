eb75e5ce1a9a45a44635d8da3919327262a35c1b
Nicola Febbrari <febbrari@gmail.com>
2023-12-18 22:27:11 +0100

    fix test

--- test/liver.test.ts
+++ test/liver.test.ts
@@ -9,7 +9,7 @@ describe('Liver test', () => {
     jest.useRealTimers();
   });
   it.each([[[30, 10]], [[20, 8]], [[40, 14]]])('test liver %p', ([isf, cr]) => {
-    const r = liver(isf, cr);
+    const r = liver(isf, cr, { physical: 1, alcohol: 0 });
     expect(r).toMatchSnapshot();
   });
 });
